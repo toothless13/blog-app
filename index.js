0a8305757c72d6b4d38725011c2a41383f088908
Tim <50870607+toothless13@users.noreply.github.com>
2024-01-30 16:59:14 +0000

    editing a post after deleting another one is not working

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from "express";
-import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+const posts = [];
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { posts: posts });
 });
 
 app.get("/create-post", (req, res) => {
@@ -16,11 +17,38 @@ app.get("/create-post", (req, res) => {
 });
 
 app.post("/create-post", (req, res) => {
-  res.sendStatus(200);
+  if (req.body.blogTitle && req.body.blogContent) {
+    req.body.postNum = posts.length;
+    posts.push(req.body);
+  } else {
+    console.log("No Content");
+  }
+  res.status(200).render("index.ejs", { posts: posts });
 });
 
-app.get("/edit-post", (req, res) => {
-  res.render("edit-post.ejs");
+app.get("/edit-post/:postId", (req, res) => {
+  // console.log(req.params);
+  const postId = req.params.postId;
+  // console.log(posts[postId]);
+  res.render("edit-post.ejs", { post: posts[postId] });
+});
+
+app.post("/edit-post", (req, res) => {
+  res.render("index.ejs", {posts: posts });
+});
+
+app.post("/edit-post/:postId", (req, res) => {
+  // console.log(req.params);
+  posts[req.params.postId].blogTitle = req.body.blogTitle;
+  posts[req.params.postId].blogContent = req.body.blogContent;
+  res.render("index.ejs", { posts: posts });
+  // res.redirect("/");
+});
+
+app.get("/remove-post/:postId", (req, res) => {
+  posts.splice(req.params.postId, 1);
+  // res.render("index.ejs", { posts: posts });
+  res.redirect("/");
 });
 
 app.listen(port, () => {
