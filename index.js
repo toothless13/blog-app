a1f69ed549bc5c6d9596b5d5317a933c3604dbb1
Tim <50870607+toothless13@users.noreply.github.com>
2024-01-31 13:03:30 +0000

    edit button works, submitting edit doesn't

--- index.js
+++ index.js
@@ -29,11 +29,24 @@ app.post("/create-post", (req, res) => {
 app.get("/edit-post/:postId", (req, res) => {
   // console.log(req.params);
   const postId = req.params.postId;
-  // console.log(posts[postId]);
-  res.render("edit-post.ejs", { post: posts[postId] });
+  console.log(postId);
+  console.log(posts);
+  const findPostIndex = post => {
+    if (postId == post.postNum) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  const postIndex = posts.findIndex(findPostIndex);
+  console.log(`Post Index is ${postIndex}`);
+  // console.log(postId);
+  console.log(posts[postIndex]);
+  // console.log(posts);
+  res.render("edit-post.ejs", { post: posts[postIndex] });
 });
 
-app.post("/edit-post", (req, res) => {
+app.get("/edit-post", (req, res) => {
   res.render("index.ejs", {posts: posts });
 });
 
@@ -47,8 +60,7 @@ app.post("/edit-post/:postId", (req, res) => {
 
 app.get("/remove-post/:postId", (req, res) => {
   posts.splice(req.params.postId, 1);
-  // res.render("index.ejs", { posts: posts });
-  res.redirect("/");
+  res.render("index.ejs", { posts: posts });
 });
 
 app.listen(port, () => {
